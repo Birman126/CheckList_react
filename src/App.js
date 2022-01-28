d5c67538fd914a2d5deeb18fd3144b2b900e9a23
Birman <birman126@gmail.com>
2022-01-28 13:35:22 +0400

    create file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+//Сделано по курсу React JS https://www.youtube.com/watch?v=xJZa2_aldDs&t=1772s
+
+import React from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, completed: true, title: "Изучить HTML" },
+    { id: 2, completed: true, title: "Изучить CSS" },
+    { id: 3, completed: true, title: "Изучить JavaScript" },
+    { id: 4, completed: false, title: "Изучить React" },
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todos.completed = !todos.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+  function addTodo(title) {
+      setTodos(
+        todos.concat([
+          {
+            title,
+            id: Date.now(),
+            completed: false,
+          },
+        ])
+      );
+    }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wraper">
+        <h1>Список дел</h1>
+        <AddTodo onCreate={addTodo}></AddTodo>
+
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
+        ) : (
+          <p>Все выполнено</p>
+        )}
+      </div>
+    </Context.Provider>
   );
 }
 
